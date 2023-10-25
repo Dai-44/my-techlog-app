aaedf9984705388738310c69209f689880f5c330
Dai <kobayashi.daigo3@gmail.com>
2023-10-25 19:02:30 +0900

    アセットファイル等はrails generate時に自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module MyTechlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
