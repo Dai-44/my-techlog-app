2feb0316eaf71d2f83b33ea0600858d766b5e478
Dai <kobayashi.daigo3@gmail.com>
2023-10-26 22:35:07 +0900

    Home#top のテストをリファクタ

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'GET /top' do
-    it 'returns http success' do
-      get '/home/top'
-      expect(response).to have_http_status(:success)
+  describe 'GET /' do
+    it 'HTTPステータス 200 を返す' do
+      get '/'
+      expect(response).to have_http_status(200)
     end
   end
 end
