e69776437d1dc8653ad15ca2dfefc3ce977738e7
Dai <kobayashi.daigo3@gmail.com>
2023-10-26 22:29:11 +0900

    Rubocopによる指摘事項修正(Lint)

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
-      get "/home/top"
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /top' do
+    it 'returns http success' do
+      get '/home/top'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
