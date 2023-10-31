19087ec12c75d89323b76a9045325dd4427e4153
Dai <kobayashi.daigo3@gmail.com>
2023-10-31 20:36:46 +0900

    ナビゲーションバーを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -12,4 +12,51 @@ RSpec.describe 'Homes', type: :system do
       expect(page).to have_content('Home#top')
     end
   end
+  
+  describe 'ナビゲーションバーの検証' do
+    context 'ログインしていない場合' do
+      before { visit '/' }
+
+      it 'ユーザー登録リンクを表示する' do
+        expect(page).to have_link('ユーザー登録', href: '/users/sign_up')
+      end
+
+      it 'ログインリンクを表示する' do
+        expect(page).to have_link('ログイン', href: '/users/sign_in')
+      end
+
+      it 'ログアウトリンクは表示しない' do
+        expect(page).not_to have_content('ログアウト')
+      end
+    end
+
+    context 'ログインしている場合' do
+      before do
+        user = create(:user) # ログイン用のユーザーを作成
+        sign_in user # 作成したユーザーでログイン
+        visit '/'
+      end
+
+      it 'ユーザー登録リンクは表示しない' do
+        expect(page).not_to have_link('ユーザー登録', href: '/users/sign_up')
+      end
+
+      it 'ログインリンクは表示しない' do
+        expect(page).not_to have_link('ログイン', href: '/users/sign_in')
+      end
+
+      it 'ログアウトリンクを表示する' do
+        expect(page).to have_content('ログアウト')
+      end
+
+      it 'ログアウトリンクが機能する' do
+        click_button 'ログアウト'
+
+        # ログインしていない状態のリンク表示パターンになることを確認
+        expect(page).to have_link('ユーザー登録', href: '/users/sign_up')
+        expect(page).to have_link('ログイン', href: '/users/sign_in')
+        expect(page).not_to have_content('ログアウト')
+      end
+    end
+  end
 end
