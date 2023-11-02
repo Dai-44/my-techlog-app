322845cae5dc76db77226a1653452ed95b120f37
Dai <kobayashi.daigo3@gmail.com>
2023-11-02 16:30:59 +0900

    feat: Google Map表示用のgemインストール, カラム作成

--- Gemfile
+++ Gemfile
@@ -53,6 +53,9 @@ gem 'bootsnap', require: false
 
 gem 'devise'
 
+gem 'gmaps4rails'
+gem 'geocoder'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_004437) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_072929) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.string "content", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
